8205aacd895d51c028ab41eb2b75efd863fd2263
juan jose sanchez mora <juanjosemjod@gmail.com>
2023-11-26 12:31:15 -0500

    fecha alterada

--- Proyecto-archivo a trabajar.py
+++ Proyecto-archivo a trabajar.py
@@ -50,6 +50,7 @@ class Inventario:
     self.ico=self.path + r'/f2.ico'
     ancho=830;alto=690 # Dimensione de la pantalla
     self.actualiza = None
+    #self.is_open_v_eliminar=None
 
     # Crea ventana principal
     self.win = tk.Tk() 
@@ -181,13 +182,15 @@ class Inventario:
 
     #Captura la fecha de compra del Producto
     self.fecha = ttk.Entry(self.frm1)
-    self.fecha.configure(width=10, foreground= "grey")
+    self.fecha.configure(width=30, foreground= "grey")
     self.fecha.place(anchor="nw", x=390, y=170)
 
-    self.fecha.insert(0, 'dd/mm/yyyy')
-    self.fecha.bind("<Key>", self.escribirFecha)
+    self.fecha.bind('<Enter>', lambda _: self.text_fecha())
+    self.fecha.bind('<Leave>', lambda _: self.borrarFecha())
+    self.fecha.bind('<Leave>', lambda _: self.win.focus())
+    self.fecha.bind("<FocusIn>", lambda _: self.borrarFecha())
+    self.fecha.bind("<KeyRelease>", self.escribirFecha)
     self.fecha.bind("<FocusOut>", self.validaFecha)
-    self.fecha.bind("<FocusIn>", self.borrarFecha)
     self.fecha.bind("<BackSpace>", lambda _:self.fecha.delete(len(self.fecha.get())),'end')
 
 
@@ -229,7 +232,6 @@ class Inventario:
     self.treeProductos.heading("Fecha",       anchor="center", text='Fecha')
 
     #Carga los datos en treeProductos
-    #self.lee_treeProductos() 
     self.treeProductos.place(anchor="nw", height=400, width=790, x=2, y=230)
 
     #Scrollbar en el eje Y de treeProductos
@@ -258,7 +260,7 @@ class Inventario:
 
     #Botón para Editar los datos
     self.btnEditar = ttk.Button(self.frm2)
-    self.btnEditar.configure(text='Editar', command=self.edit_Button)
+    self.btnEditar.configure(text='Editar', command=self.carga_Datos)
     self.btnEditar.place(anchor="nw", width=70, x=350, y=20)
 
     #Botón para Elimnar datos
@@ -268,7 +270,7 @@ class Inventario:
 
     #Botón para cancelar una operación
     self.btnCancelar = ttk.Button(self.frm2)
-    self.btnCancelar.configure(text='Cancelar', width=80, command = self.cancel_Button)
+    self.btnCancelar.configure(text='Cancelar', width=80, command = self.cancel_Button_Main_Win)
     self.btnCancelar.place(anchor="nw", width=70, x=500, y=20)
 
     #Ubicación del Frame 2
@@ -281,7 +283,7 @@ class Inventario:
   #Fución de manejo de eventos del sistema
   
   def database_build(self):
-     producto=''' CREATE TABLE IF NOT EXISTS "Producto" (
+     producto=''' CREATE TABLE IF NOT EXISTS "Productos" (
 	  "IdNit"	VARCHAR(15),
 	  "Codigo"	VARCHAR(15) NOT NULL,
 	  "Descripcion"	VARCHAR,
@@ -290,7 +292,7 @@ class Inventario:
 	  "Precio"	DOUBLE,
 	  "Fecha"	DATE,
 	  PRIMARY KEY("Codigo","IdNit") ) '''
-     proveedor='''CREATE TABLE IF NOT EXISTS "Proveedor" (
+     proveedor='''CREATE TABLE IF NOT EXISTS "Proveedores" (
 	  "idNitProv"	VARCHAR NOT NULL UNIQUE,
 	  "Razon_Social"	VARCHAR,
 	  "Ciudad"	VARCHAR,
@@ -310,17 +312,7 @@ class Inventario:
       y = win.winfo_screenheight() // 2 - alto // 2 
       win.geometry(f'{ancho}x{alto}+{x}+{y}') 
       win.deiconify() # Se usa para restaurar la ventana
-      
-  def estado_Buttons(self, estado):
-     if estado==True:
-        self.btnBuscar.configure(state='normal')
-        self.btnEditar.configure(state='normal')
-        self.btnEliminar.configure(state='normal')
-     elif estado==False:
-        self.btnBuscar.configure(state='disabled')
-        self.btnEditar.configure(state='disabled')
-        self.btnEliminar.configure(state='disabled')
-
+   
   def abrir_Ventana_Treeview(self):
      # Crear una ventana secundaria usando toplevel
      self.ventana_Treeview = tk.Toplevel(self.win)
@@ -359,7 +351,6 @@ class Inventario:
      self.tree.heading("Fecha",       anchor="center", text='Fecha')
 
      #Carga los datos en treeProductos
-     #self.lee_treeProductos() 
      self.tree.place(anchor="nw", height=400, width=790, x=2, y=230)
 
      #Scrollbar en el eje Y de treeProductos
@@ -459,14 +450,45 @@ class Inventario:
   def validaFecha(self, event):  
       '''Valida que la fecha sea válida'''
       if event.char:
-         if es_Fecha_Valida(self.fecha.get()) == False:
+         if es_Fecha_Valida(self.fecha.get()) == False and self.fecha.get()!= '':
             mssg.showerror('Atención!!','.. ¡Fecha Inválida! ..')
             self.fecha.delete(0, tk.END)
+
+  def escribirFecha(self, event):
+    #para que solo se ejecuute si escribe números y tiene menos de diez caracteres
+    if event.char.isdigit() and len(self.fecha.get())<10:
+        
+        texto = self.fecha.get()
+        letras = 0
+        for i in texto:
+            letras +=1
+
+        if letras == 2:
+            self.fecha.insert(2,"/")
+        elif letras == 5:
+            self.fecha.insert(5,"/")
+    else:
+      return 'break'
+    #se usa el return break para evitar que el evento se propague
+
+  def borrarFecha(self):
+
+   if self.fecha.get()=="dd/mm/yyyy":
+      self.fecha.configure(state="normal")
+      self.fecha.delete(0, tk.END)
+      self.fecha.config(foreground='black')
+
+  def text_fecha(self):
+     if self.fecha.get()=="":
+        self.fecha.configure(foreground='gray')
+        self.fecha.insert(0,"dd/mm/yyyy")
+        self.fecha.configure(state='readonly')
+
 
    
   def validar_ID(self):
      id=self.idNit.get()
-     search_id=self.accion_Buscar('*','Proveedor','idNitProv= ? ', (id,)).fetchone()
+     search_id=self.accion_Buscar('*','Proveedores','idNitProv= ? ', (id,)).fetchone()
      if search_id==None :
         prov_Exist=False
      else:
@@ -475,7 +497,7 @@ class Inventario:
   
   def validar_Cod(self):
      codigo=self.codigo.get()
-     search_id=self.accion_Buscar('*','Producto','Codigo= ? ', (codigo,)).fetchone()
+     search_id=self.accion_Buscar('*','Productos','Codigo= ? ', (codigo,)).fetchone()
      if search_id==None :
         cod_Exist=False
      else:
@@ -526,27 +548,27 @@ class Inventario:
      self.run_Query(delete, valores_de_la_condicion)
   
   def insertar_Proveedor(self, id , razon_social, ciudad):
-    insert=f''' INSERT INTO Proveedor VALUES (?,?,?)'''
+    insert=f''' INSERT INTO Proveedores VALUES (?,?,?)'''
     self.run_Query(insert, (id, razon_social, ciudad,))
    
   def insertar_Producto(self, IdNit, Codigo, descripcion, und, cantidad, precio, fecha):
-    insert=f''' INSERT INTO Producto VALUES (?,?,?,?,?,?,?)'''
+    insert=f''' INSERT INTO Productos VALUES (?,?,?,?,?,?,?)'''
     self.run_Query(insert, (IdNit, Codigo, descripcion, und, cantidad, precio, fecha,))
 
   def actualizar_Proveedor(self, values):
-     update=''' UPDATE Proveedor 
+     update=''' UPDATE Proveedores 
                 SET Razon_Social = ? , Ciudad = ? WHERE 
                 idNitProv = ? '''
      self.run_Query(update,values)
 
   def actualizar_Producto(self,values):
-     update=''' UPDATE Producto 
+     update=''' UPDATE Productos 
              SET Descripcion = ? , Und = ? , Cantidad = ? , Precio = ? , Fecha = ? 
              WHERE IdNit = ? AND Codigo = ? '''
      self.run_Query(update,values)
   
   def cargar_Proveedor(self, id):
-     proveedor=self.accion_Buscar("*","Proveedor","idNitProv= ? " , (id,)).fetchone()
+     proveedor=self.accion_Buscar("*","Proveedores","idNitProv= ? " , (id,)).fetchone()
      self.idNit.insert(0,proveedor[0])
      self.razonSocial.insert(0,proveedor[1])
      self.ciudad.insert(0, proveedor[2])
@@ -565,7 +587,7 @@ class Inventario:
      cod= self.codigo.get()
      if id != "" and cod =="":
         if self.validar_ID()==True:
-          search=self.accion_Buscar("*","Producto", "IdNit= ? ", (id,)).fetchall()
+          search=self.accion_Buscar("*","Productos", "IdNit= ? ", (id,)).fetchall()
           self.limpia_Campos()
           self.cargar_Datos_Buscados(search)
           self.cargar_Proveedor(id)
@@ -573,7 +595,7 @@ class Inventario:
            mssg.showerror('Atención!!','.. ¡El proveedor no existe! ..')
      elif id == "" and cod !="":
         if self.validar_Cod()==True:
-           search=self.accion_Buscar("*","Producto", "Codigo= ? ",(cod,)).fetchall()
+           search=self.accion_Buscar("*","Productos", "Codigo= ? ",(cod,)).fetchall()
            self.cargar_Datos_Buscados(search)
            self.limpia_Campos()
            if len(search)>1:
@@ -585,7 +607,7 @@ class Inventario:
            mssg.showerror('Atención!!','.. ¡El producto no existe! ..')
      elif id != "" and cod !="":
         if self.validar_ID()==True and self.validar_Cod()==True:
-           search=self.accion_Buscar("*","Producto", "Codigo= ? AND IdNit= ? ", (cod , id,)).fetchall()
+           search=self.accion_Buscar("*","Productos", "Codigo= ? AND IdNit= ? ", (cod , id,)).fetchall()
            if search == None:
               mssg.showerror('Atención!!','.. ¡El producto no corresponde al proveedor indicado! ..')
            else: 
@@ -604,20 +626,20 @@ class Inventario:
       self.limpiar_Treeview()
       self.cargar_Datos_Treeview(search)
 
-  def cancel_Button(self):
+  def cancel_Button_Main_Win(self):
      self.limpiar_Treeview()
      self.limpia_Campos()
      if self.actualiza== True:
          self.actualiza = None
          mssg.showinfo(".. Confirmación ..", '.. Ha salido del modo Editar ..')
-     self.estado_Buttons(True)
+     self.estado_Buttons_Eliminar(True)
    
   def record_Button(self):
      if self.actualiza==None:
         self.adiciona_Registro()
      elif self.actualiza== True:
         self.actualizar_datos()
-        self.estado_Buttons(True)
+        self.estado_Buttons_Editar(True)
 
   def adiciona_Registro(self, event=None):
     '''Adiciona un producto a la BD si la validación es True'''
@@ -631,7 +653,7 @@ class Inventario:
     cantidad = self.cantidad.get()
     precio = self.precio.get()
     fecha = self.fecha.get()
-    relacion=self.accion_Buscar('*','Producto','IdNit = ? AND Codigo = ?',(id_nit, codigo,)).fetchone()
+    relacion=self.accion_Buscar('*','Productos','IdNit = ? AND Codigo = ?',(id_nit, codigo,)).fetchone()
 
     if id_nit=="" and codigo=="":
        mssg.showerror('Atención!!','.. ¡No se han especificado ningun dato para guardar! ..')
@@ -655,14 +677,14 @@ class Inventario:
              mssg.showinfo('Confirmación','.. El proveedor a sido registrado correctamente ..')
              self.insertar_Producto(id_nit,codigo,descripcion,unidad,cantidad, precio, fecha)
              mssg.showinfo('Confirmación','.. El producto a sido registrado correctamente ..')
-             datos=self.accion_Buscar("*","Producto"," IdNit = ? AND Codigo = ?",(id_nit,codigo,)).fetchall()
+             datos=self.accion_Buscar("*","Productos"," IdNit = ? AND Codigo = ?",(id_nit,codigo,)).fetchall()
              self.cargar_Datos_Buscados(datos)
 
           elif (self.validar_ID()==True and self.validar_Cod()==False) or (relacion == None):
           
              self.insertar_Producto(id_nit,codigo,descripcion,unidad,cantidad, precio, fecha)
              mssg.showinfo('Confirmación','.. El producto a sido registrado correctamente ..')
-             datos=self.accion_Buscar("*","Producto"," IdNit = ? AND Codigo = ?",(id_nit,codigo,)).fetchall()
+             datos=self.accion_Buscar("*","Productos"," IdNit = ? AND Codigo = ?",(id_nit,codigo,)).fetchall()
              self.cargar_Datos_Buscados(datos)
 
           elif relacion!= None:  
@@ -670,10 +692,10 @@ class Inventario:
        elif fecha=="":
             mssg.showerror('Atención!!','.. ¡Digite una fecha para registrar! ..')
   
-  def edit_Button(self):
+  def carga_Datos(self):
     seleccion=self.treeProductos.selection()
     if seleccion != ():
-       self.estado_Buttons(False)
+       self.estado_Buttons_Editar(False)
        self.limpia_Campos()
        item=self.treeProductos.item(seleccion)
        self.cargar_Proveedor(item ['text'])
@@ -693,6 +715,16 @@ class Inventario:
     elif seleccion== ():
        mssg.showerror('Atención!!','.. ¡No se ha seleccionado nada! ..')
 
+  def estado_Buttons_Editar(self, estado):
+     if estado==True:
+        self.btnBuscar.configure(state='normal')
+        self.btnEditar.configure(state='normal')
+        self.btnEliminar.configure(state='normal')
+     elif estado==False:
+        self.btnBuscar.configure(state='disabled')
+        self.btnEditar.configure(state='disabled')
+        self.btnEliminar.configure(state='disabled')
+
   def actualizar_datos(self):
      id_nit = self.idNit.get()
      razon_social = self.razonSocial.get()
@@ -703,8 +735,8 @@ class Inventario:
      cantidad = float(self.cantidad.get())
      precio = float(self.precio.get())
      fecha = self.fecha.get()
-     proveedor=self.accion_Buscar('*', 'Proveedor', ' idNitProv = ? ', (id_nit,)).fetchone()
-     producto=self.accion_Buscar('*', 'Producto', ' IdNit = ? AND Codigo = ? ', (id_nit, codigo,)).fetchone()
+     proveedor=self.accion_Buscar('*', 'Proveedores', ' idNitProv = ? ', (id_nit,)).fetchone()
+     producto=self.accion_Buscar('*', 'Productos', ' IdNit = ? AND Codigo = ? ', (id_nit, codigo,)).fetchone()
      proveedor_update=(id_nit,razon_social,ciudad)
      producto_update=(id_nit,codigo,descripcion,unidad,cantidad,precio,fecha)
      if proveedor == proveedor_update:
@@ -725,10 +757,27 @@ class Inventario:
      self.limpia_Campos()
 
   def eliminar_Button(self):
-     if self.treeProductos.selection() != ():
+     if self.treeProductos.selection() != (): #and self.is_open_v_eliminar==None:
+        #self.is_open_v_eliminar=True
+        self.estado_Buttons_Eliminar(False)
         self.abrir_Ventana_Eliminar()
+        '''elif self.is_open_v_eliminar==True:
+        mssg.showerror('.. Error! ..', ' Ya se encuentra abierta una ventana de eliminación!')
+        self.ventana_eliminar.lift(self.win)'''
      elif self.treeProductos.selection()==():
         mssg.showerror('.. Error!..', ' .. No se ha seleccionado ningun registro a eliminar! .. ')
+
+  def estado_Buttons_Eliminar(self,estado):
+     if estado==False:
+        self.btnBuscar.configure(state='disabled')
+        self.btnGrabar.configure(state='disabled')
+        self.btnEditar.configure(state='disabled')
+        self.btnEliminar.configure(state='disabled')
+     elif estado==True:
+        self.btnBuscar.configure(state='normal')
+        self.btnGrabar.configure(state='normal')
+        self.btnEditar.configure(state='normal')
+        self.btnEliminar.configure(state='normal')
 
   def elimina_Registro(self, obj):
     '''Elimina un Registro en la BD'''
@@ -736,22 +785,22 @@ class Inventario:
     codigo=obj["values"][0]
     if self.obj_eliminar.get()=='Producto':
        try:
-          self.accion_Eliminar('Producto','IdNit = ? AND Codigo = ? ',(id, codigo,))
+          self.accion_Eliminar('Productos','IdNit = ? AND Codigo = ? ',(id, codigo,))
        except:
           mssg.showerror('.. Error! ..', f'Se ha producido un error al tratar de eliminar el registro del producto {codigo}, correspondiente al proveedor {id}')
        else:
           mssg.showinfo(' .. Confirmación .. ', f'Se ha eliminado el registro del producto {codigo}, correspondiente al proveedor {id} correctamente')
     elif self.obj_eliminar.get()=='Proveedor':
        try:
-          self.accion_Eliminar('Producto','IdNit = ? ',(id,))
-          self.accion_Eliminar('Proveedor','idNitProv = ? ', (id,))
+          self.accion_Eliminar('Productos','IdNit = ? ',(id,))
+          self.accion_Eliminar('Proveedores','idNitProv = ? ', (id,))
        except:
           mssg.showerror('.. Error! ..', f'Se ha producido un error al tratar de eliminar el proveedor {id} y todos los registros relacionados a él')
        else:
           mssg.showinfo(' .. Confirmación .. ', f'Se ha eliminado el proveedor {id} y todos los registros relacionados a él correctamente')
     elif self.obj_eliminar.get()=='Todos los productos':
        try:
-          self.accion_Eliminar('Producto','Codigo = ? ',(codigo,))
+          self.accion_Eliminar('Productos','Codigo = ? ',(codigo,))
        except:
           mssg.showerror('.. Error! ..', f'Se ha producido un error al tratar de eliminar todos los registros relacionados con el producto {codigo}')
        else:
@@ -764,7 +813,7 @@ class Inventario:
             background="#afafaf",
             padx=10,
             pady=30)
-     self.ventana_eliminar.geometry("350x350")
+     self.ventana_eliminar.protocol("WM_DELETE_WINDOW", lambda: None)
      self.ventana_eliminar.iconbitmap(self.ico)
      self.ventana_eliminar.resizable(False, False)
      self.ventana_eliminar.title("Eliminar")
@@ -801,8 +850,9 @@ class Inventario:
      self.radiobutton3.place(anchor="w", x=10, y=145)
     
      self.btn_cancelar= ttk.Button(self.ventana_eliminar)
-     self.btn_cancelar.configure(text='Cancelar', command= self.ventana_eliminar.destroy)
+     self.btn_cancelar.configure(text='Cancelar')
      self.btn_cancelar.pack(side="bottom")
+     self.btn_cancelar.bind('<Button-1>', lambda _: self.salir_ventana_eliminar())
 
      self.btn_continuar = ttk.Button(self.ventana_eliminar)
      self.btn_continuar.configure(text='Continuar', command= self.continuar_Button)
@@ -810,6 +860,14 @@ class Inventario:
 
      # Centrar la ventana emergente en la pantalla
      self.centra(self.ventana_eliminar, 350, 350)
+
+  def salir_ventana_eliminar(self):
+     self.estado_Buttons_Eliminar(True)
+     self.ventana_eliminar.destroy()
+     self.limpiar_Treeview()
+     self.limpia_Campos()
+     #self.is_open_v_eliminar=None
+     
 
   def continuar_Button(self):
      item=self.treeProductos.item(self.treeProductos.selection())
@@ -830,6 +888,8 @@ class Inventario:
             padx=10,
             pady=10,
             width=500)
+     self.ventana_confirmacion.protocol("WM_DELETE_WINDOW",lambda: None)
+     self.ventana_confirmacion.grab_set()
      self.ventana_confirmacion.iconbitmap(self.ico)
      self.ventana_confirmacion.resizable(False, False)
      self.ventana_confirmacion.title("Warning")
@@ -850,38 +910,26 @@ class Inventario:
      self.btn_yes = ttk.Button(self.ventana_confirmacion)
      self.btn_yes.configure(text='Si')
      self.btn_yes.place(anchor="w", width=70, x=243, y=150)
-     self.btn_yes.bind('<Button-1>', lambda _: (self.elimina_Registro(item),self.ventana_eliminar.destroy()))
+     self.btn_yes.bind('<Button-1>', lambda _: self.btn_Yes(item) )
 
      self.btn_no = ttk.Button(self.ventana_confirmacion)
-     self.btn_no.configure(text='No', command= self.ventana_eliminar.destroy)
+     self.btn_no.configure(text='No')
      self.btn_no.place(anchor="w", x=317, y=150)
+     self.btn_no.bind('<Button-1>', lambda _: self.btn_No())
      
      self.centra(self.ventana_confirmacion,500,200)
 
-  def escribirFecha(self, event):
-    #para que solo se ejecuute si escribe números y tiene menos de diez caracteres
-    if event.char.isdigit() and len(self.fecha.get())<10:
-        
-        texto = self.fecha.get()
-        letras = 0
-        for i in texto:
-            letras +=1
-
-        if letras == 2:
-            self.fecha.insert(2,"/")
-        elif letras == 5:
-            self.fecha.insert(5,"/")
-    else:
-      return 'break'
-    #se usa el return break para evitar que el evento se propague
-
-  def borrarFecha(self,event):
-
-   if self.fecha.get() == "dd/mm/yyyy":
-      self.fecha.delete(0, tk.END)
-      self.fecha.config(foreground='black')
-
+  def btn_Yes (self,objeto_eliminar):
+     self.elimina_Registro(objeto_eliminar)
+     self.ventana_confirmacion.grab_release()
+     self.ventana_confirmacion.destroy()
+     self.salir_ventana_eliminar()
      
+   
+  def btn_No(self):
+     self.ventana_confirmacion.grab_release()
+     self.ventana_confirmacion.destroy()
+
 if __name__ == "__main__":
     app = Inventario()
     app.run()
